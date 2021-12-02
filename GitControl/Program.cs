38c78ff2d2fa4c088eb93fce52195543ebd1d5ff
andreikabig <apps.bigint@gmail.com>
2021-12-02 11:26:08 +0300

    FirstCommit

---
+++ GitControl/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitControl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp = new() { Id = 1, Name = "Andrey" };
+        }
+    }
+}
