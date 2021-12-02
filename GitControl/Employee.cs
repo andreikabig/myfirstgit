fea0d8dee44c5d186cee91720ceb01a0d0df15ed
Andrey Dyudkin <apps.bigint@gmail.com>
2021-12-02 12:33:49 +0300

    Added DateTime to Employee

--- GitControl/Employee.cs
+++ GitControl/Employee.cs
@@ -11,5 +11,6 @@ namespace GitControl
         public int Id { get; set; }
         public string Name { get; set; }
         public string Place { get; set; }
+        public DateTime DateTime { get; set; }
     }
 }
